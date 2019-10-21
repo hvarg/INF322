144c7966d146ab8b0003cca64c454f010b0a4fda
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2019-10-21 19:13:40 -0300

    Deleted unused content

--- src/store.ts
+++ src/store.ts
@@ -27,20 +27,14 @@ import thunk, { ThunkMiddleware } from 'redux-thunk';
 import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
 
 import app, { AppState } from './reducers/app.js';
-import { CounterState } from './reducers/counter.js';
-import { ShopState } from './reducers/shop.js';
 import { AppAction } from './actions/app.js';
-import { CounterAction } from './actions/counter.js';
-import { ShopAction } from './actions/shop.js';
 
 // Overall state extends static states and partials lazy states.
 export interface RootState {
   app?: AppState;
-  counter?: CounterState;
-  shop?: ShopState;
 }
 
-export type RootAction = AppAction | CounterAction | ShopAction;
+export type RootAction = AppAction;
 
 // Sets up a Chrome extension for time travel debugging.
 // See https://github.com/zalmoxisus/redux-devtools-extension for more information.
